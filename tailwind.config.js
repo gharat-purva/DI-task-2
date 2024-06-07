3d8b91923955795664bac7bf7a3852c110bbecea
PURVA <1032210421@tcetmumbai.in>
2024-06-07 16:40:53 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      },
+      colors: {
+        placeholder: '#c4c4c4',
+      },
+    },
+  },
+  plugins: [],
+}
