206a68f142cd60d25d2b596d9899830ec05c0d57
PURVA <1032210421@tcetmumbai.in>
2024-07-30 19:05:19 +0530

    cyber and beyond logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
+import logoDesktop from '../assets/logoDesktop.png';
+import logoMobile from '../assets/logoMobile.png';
 
 function Header() {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   return (
     <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
       <div className="text-2xl font-serif font-bold">WARTA</div>
+      {isMobile ? (
+        <img src={logoMobile} alt="Logo" style={{ width: '25px' }} />
+      ) : (
+        <img src={logoDesktop} alt="Logo" style={{ width: '150px' }} />
+      )}
       <nav className="flex space-x-8">
         <a href="#home" className="hover:text-gray-400">Home</a>
         <a href="#news" className="hover:text-gray-400">News</a>
